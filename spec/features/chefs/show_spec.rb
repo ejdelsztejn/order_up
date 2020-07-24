10e662cb2aa71a6ef16e9d8032f52aee570b9bb4
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-24 12:15:25 -0400

    Add chef show page spec

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "dish show page", type: :feature do
+RSpec.describe "chef show page", type: :feature do
   before(:each) do
     @naftali = Chef.create!(name: "Naftali Schaechter")
 
@@ -27,24 +27,32 @@ RSpec.describe "dish show page", type: :feature do
     DishIngredient.create!(dish: @kishke, ingredient: @schmaltz)
     DishIngredient.create!(dish: @kishke, ingredient: @matzo_meal)
   end
-  describe "When I visit a dish's show page" do
-    it "I see a list of ingredients for that dish and the chef's name" do
-      visit "/dish/#{@lokshn_kugel.id}"
-
-      expect(page).to have_content(@lokshn_kugel.name)
-      expect(page).to have_content(@noodles.name)
-      expect(page).to have_content(@eggs.name)
-      expect(page).to have_content(@cottage_cheese.name)
-      expect(page).to have_content(@sugar.name)
-      expect(page).to have_content(@cinnamon.name)
+  describe "When I visit a chef's show page" do
+    it "I see the name of that chef" do
+      visit "/chef/#{@naftali.id}"
       expect(page).to have_content(@naftali.name)
     end
-    it "I see the total calorie count for that dish" do
-      visit "/dish/#{@lokshn_kugel.id}"
-      expect(page).to have_content("Calories: #{@lokshn_kugel.total_calories}")
+    it "I see a link to view a list of all ingredients that this chef uses in their dishes" do
+      visit "/chef/#{@naftali.id}"
+      expect(page).to have_link("View All Ingredients Used by #{@naftali.name}")
+    end
+    describe "When I click on that link" do
+      it "I'm taken to a chef's ingredient index page" do
+        visit "/chef/#{@naftali.id}"
 
-      visit "/dish/#{@kishke.id}"
-      expect(page).to have_content("Calories: #{@kishke.total_calories}")
+        click_on("View All Ingredients Used by #{@naftali.name}")
+        expect(page).to eq("/chef/#{@naftali}.id/ingredients")
+      end
+      it "I can see a unique list of names of all the ingredients that this chef uses" do
+        visit("/chef/#{@naftali}.id/ingredients")
+
+        expect(page).to have_content(@noodles.name)
+        expect(page).to have_content(@eggs.name)
+        expect(page).to have_content(@cottage_cheese.name)
+        expect(page).to have_content(@sugar.name)
+        expect(page).to have_content(@cinnamon.name)
+        expect(page).to have_content(@naftali.name)
+      end
     end
   end
 end
