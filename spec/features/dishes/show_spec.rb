89007ae9be3f5cd35f019b67aa0b65bff4f7c88d
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-24 11:26:02 -0400

    Add initial set up for dish show page spec

---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe "dish show page", type: :feature do
+  describe "When I visit a dish's show page" do
+    it "I see a list of ingredients for that dish and the chef's name" do
+    end
+  end
+end
